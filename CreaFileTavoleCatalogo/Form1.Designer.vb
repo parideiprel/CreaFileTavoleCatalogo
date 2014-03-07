23ad1a9ef1e4968aea459c45ed91f054f31271c0
Paride Balestri IPREL <paride_balestri@iprel.it>
2014-03-07 10:25:36 +0100

    Release

---
+++ CreaFileTavoleCatalogo/Form1.Designer.vb
@@ -0,0 +1,185 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla nell'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Blue
+        Me.Label1.Location = New System.Drawing.Point(160, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(327, 27)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Generatore di tavole per Catalogo"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(21, 87)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(109, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(18, 68)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(112, 16)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Codice Quadro"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(162, 68)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(138, 16)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Tavola di partenza"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(206, 87)
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(51, 20)
+        Me.NumericUpDown1.TabIndex = 6
+        Me.NumericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.Location = New System.Drawing.Point(362, 87)
+        Me.NumericUpDown2.Maximum = New Decimal(New Integer() {99, 0, 0, 0})
+        Me.NumericUpDown2.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(51, 20)
+        Me.NumericUpDown2.TabIndex = 8
+        Me.NumericUpDown2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.NumericUpDown2.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(328, 68)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(118, 16)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Tavola di arrivo"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(469, 68)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(137, 16)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Estensione Cliente"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(472, 87)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(134, 20)
+        Me.TextBox2.TabIndex = 9
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Yellow
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(265, 135)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(98, 39)
+        Me.Button1.TabIndex = 11
+        Me.Button1.Text = "Genera"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(21, 217)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(585, 23)
+        Me.ProgressBar1.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(640, 283)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.NumericUpDown2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "GestioneTavole"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown2 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+
+End Class
---
+++ CreaFileTavoleCatalogo/Form1.vb
@@ -0,0 +1,164 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+
+
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        'Definizione variabili
+        Dim iIdx As Integer = 0
+        Dim fsIn As FileStream
+        Dim binStreamReader As BinaryReader
+        Dim binIn As Array
+
+        Dim fsOut As FileStream
+
+        Dim sPre As String
+        Dim iStart As Integer
+        Dim iEnd As Integer
+        Dim sPost As String
+
+        Dim sTavola As String
+        Select Case TextBox1.Text
+            Case Is = ""
+                MsgBox("Inserire codice quadro!", vbExclamation & vbOKOnly, "Dati mancanti")
+                If TextBox1.Text = "" Then
+                    TextBox1.BackColor = Color.Red
+                End If
+                TextBox1.Focus()
+                Exit Sub
+            Case Is <> ""
+                TextBox1.BackColor = Color.White
+                TextBox1.Text = UCase(TextBox1.Text)
+            Case Else
+                Exit Select
+        End Select
+
+        Select Case TextBox2.Text
+            Case Is = ""
+                MsgBox("Inserire estensione!", vbExclamation & vbOKOnly, "Dati mancanti")
+                If TextBox2.Text = "" Then
+                    TextBox2.BackColor = Color.Red
+                End If
+                TextBox2.Focus()
+                Exit Sub
+            Case Is <> ""
+                TextBox2.BackColor = Color.White
+            Case Else
+                Exit Select
+        End Select
+
+
+
+        With ProgressBar1
+            .Minimum = iStart
+            .Maximum = iEnd
+            .ForeColor = Color.Blue
+            .Step = 1
+        End With
+
+        Try
+            fsIn = New FileStream(Application.StartupPath & "\Matrici\Tavola_Bianca.psd", FileMode.Open)
+
+            binStreamReader = New BinaryReader(fsIn)
+            binIn = binStreamReader.ReadBytes(Convert.ToInt32(fsIn.Length))
+            binStreamReader.Close()
+            fsIn.Close()
+
+            Dim a = FolderBrowserDialog1.ShowDialog()
+            If a = Windows.Forms.DialogResult.Cancel Then
+                MsgBox("Generazione annullata", vbOKOnly, "Intervento Utente")
+                Exit Sub
+            End If
+
+            'compongo il nome del file
+            sPre = TextBox1.Text
+            iStart = NumericUpDown1.Value
+            iEnd = NumericUpDown2.Value
+            sPost = TextBox2.Text
+
+            For iIdx = iStart To iEnd
+
+                ProgressBar1.PerformStep()
+
+                'If iEnd > 9 Then
+                If iEnd > 9 And iIdx < 10 Then
+                    sTavola = sPre & "A" & "0" & iIdx.ToString & iEnd.ToString & "IJ" & sPost & ".psd"
+                Else
+                    sTavola = sPre & "A" & iIdx.ToString & iEnd.ToString & "IJ" & sPost & ".psd"
+                End If
+
+
+                fsOut = New FileStream(FolderBrowserDialog1.SelectedPath & "\" & sTavola, FileMode.Create)
+                fsOut.Write(binIn, 0, binIn.Length)
+                fsOut.Close()
+
+            Next
+
+
+
+        Catch ex As Exception
+            MsgBox("Exception: " & ex.Message)
+            
+            If fsIn.SafeFileHandle.IsClosed = False Then
+                fsIn.Close()
+                fsIn.Dispose()
+            Else
+                fsIn.Dispose()
+            End If
+
+            If fsOut.SafeFileHandle.IsClosed = False Then
+                fsOut.Close()
+                fsOut.Dispose()
+            Else
+                fsOut.Dispose()
+            End If
+
+            Exit Sub
+
+
+        End Try
+
+        MsgBox("Generazione Terminata !!", vbExclamation & vbOKOnly, "Esito Generazione")
+        '        If fsIn.SafeFileHandle.IsInvalid = False Then
+        'fsIn.Close()
+        fsIn.Dispose()
+        'Else
+        'fsIn.Dispose()
+        'End If
+
+        'If fsOut.SafeFileHandle.IsClosed = False Then
+        'fsOut.Close()
+        fsOut.Dispose()
+        'Else
+        'fsOut.Dispose()
+        'End If
+
+        '        fsIn.Dispose()
+        '        fsOut.Dispose()
+        End
+
+        'Function LoadBinaryData(ByVal path As String) As Object
+        '        ' Open a file stream for input
+        '        Dim fs As FileStream = New FileStream(path, FileMode.Open)
+        '        ' Create a binary formatter for this stream
+        '        Dim bf As New BinaryFormatter()
+
+        '        ' Deserialize the contents of the file stream into an object
+        '        LoadBinaryData = bf.Deserialize(fs)
+        '        ' close the stream.
+        '        fs.Close()
+        '    End Function
+
+        '        FileStream fs = new FileStream(filePath, FileMode.Open);
+        'BinaryReader br = new BinaryReader(fs);
+        'byte[] bin = br.ReadBytes(Convert.ToInt32(fs.Length));
+        'fs.Close();
+        'br.Close();
+
+
+
+    End Sub
+End Class
